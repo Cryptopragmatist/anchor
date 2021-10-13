5aeb34599090647fce940d680bbf88cb20342331
Cryptopragmatic <46362910+Cryptopragmatist@users.noreply.github.com>
2021-10-13 18:27:00 +0800

    Add files via upload
    creating a client

---
+++ client.js
@@ -0,0 +1,28 @@
+// client.js is used to introduce the reader to generating clients from IDLs.
+// It is not expected users directly test with this example. For a more
+// ergonomic example, see `tests/basic-0.js` in this workspace.
+
+//const anchor = require('./Programming/Rust2/anchorclone/anchor-1');
+
+const anchor = require('@project-serum/anchor');
+// Configure the local cluster.
+anchor.setProvider(anchor.Provider.local());
+
+async function main() {
+  // #region main
+  // Read the generated IDL.
+  const idl = JSON.parse(require('fs').readFileSync('./target/idl/basic_0.json', 'utf8'));
+
+  // Address of the deployed program.
+  const programId = new anchor.web3.PublicKey('BSx34g4SKLYBrVdGuaw7xJz1yY6dLRhz9r2ZwwQjM1WY');
+
+  // Generate the program client from IDL.
+  const program = new anchor.Program(idl, programId);
+
+  // Execute the RPC.
+  await program.rpc.initialize();
+  // #endregion main
+}
+
+console.log('Running client.');
+main().then(() => console.log('Success'));
